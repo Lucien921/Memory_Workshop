c0d569e6fab65b3a79c242da8d36f10d59464457
lucien <lucien.gaudinot@epitech.eu>
2023-05-29 18:19:58 +0200

    [ADD] 4th tache finished

--- includes/malloc.h
+++ includes/malloc.h
@@ -5,7 +5,18 @@
 ** malloc
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #ifndef MALLOC_H_
     #define MALLOC_H_
+
+typedef struct metadata {
+    void *adress;
+    size_t size;
+    struct metadata* next;
+    int is_free;
+} metadata_t;
 
 #endif /* !MALLOC_H_ */
--- sources/main.c
+++ sources/main.c
@@ -5,31 +5,43 @@
 ** memory
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "../includes/malloc.h"
+
+metadata_t *insert_metadata_to_ll(metadata_t *current_block, metadata_t *block_metadata)
+{
+    while (current_block->next != NULL) {
+        current_block = current_block->next;
+    }
+    current_block->next = block_metadata;
+    return current_block;
+}
 
 void *my_malloc(size_t size)
 {
-    const void *heap_start = sbrk(0);
-    static size_t heap_size = 0;
+    metadata_t *current_block = sbrk(0);
     void *allocated_block = sbrk(size);
+    metadata_t *block_metadata = (metadata_t *)allocated_block;
 
-    if (allocated_block == (void *)-1) {
+    block_metadata->size = size;
+    block_metadata->next = NULL;
+    block_metadata->is_free = 0;
+    if (block_metadata == (void *)-1) {
         printf("Segmentation fault.\n");
         return NULL;
     }
-
-    heap_size += size;
-    return allocated_block;
+    if (current_block != NULL) {
+        insert_metadata_to_ll(current_block, block_metadata);
+    } else
+        current_block = block_metadata;
+    return (void *)(block_metadata + 1);
 }
 
 int main(int argc, char *argv[])
 {
     size_t size = (size_t)strtof(argv[1], NULL);
     void *alloc_mem = my_malloc(size);
-    if (alloc_mem != NULL)
-        printf("memory allowed in adress: %p.\n", alloc_mem);
+    if (alloc_mem != NULL) {
+        printf("Memory allocated at address: %p.\n", alloc_mem);
+    }
     return 0;
 }
